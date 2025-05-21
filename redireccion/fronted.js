038f3609293c8c871db8764ef247b3039be8a819
juannr1 <juanfelipe1909@outlook.com>
2025-05-21 17:30:22 -0500

    cambio
    servidor-loader and botones de paginas principal

--- redireccion/fronted.js
+++ redireccion/fronted.js
@@ -2,75 +2,49 @@
 
   // Consulta al backend para obtener precio, icono y servicio del div
 
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const loaderContainer = document.getElementById("loader-container");
+  
+  
+  
+  loaderContainer.style.display = "flex";
+  document.body.style.overflow = "hidden"; 
+  
+  let peticionesCompletadas = 0;
+  const totalPeticiones = 3;
+  
+  
+  function verificarPeticiones() {
+    peticionesCompletadas++;
+    console.log(`Petición completada. Total: ${peticionesCompletadas}/${totalPeticiones}`);
+    
+    
+    if (peticionesCompletadas === totalPeticiones) {
+      
+  
+      
+      
+      setTimeout(() => {
+        loaderContainer.style.display = "none";
+        document.body.style.overflow = "auto"; // Restaurar scroll
+        console.log("Todas las peticiones completadas - Loader ocultado");
+      }, 500);
+    }
+  }
+
+
+
+
   // modulo 1
-
-   axios.get('http://localhost:3000/planes/1')
-  .then(response => {
-      
-    const valores_Convertidos = response.data;
-    document.getElementById("plan1").innerHTML = valores_Convertidos.Plan;
-
-
-    // manejar respuesta exitosa
-    console.log("success");
-
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  });
-
-
-
-    axios.get('http://localhost:3000/precio/1')
-  .then(response => {
-      
-    const valores_Convertidos = response.data;
-    document.getElementById("resultado").textContent = valores_Convertidos.Precio;
-
-
-    // manejar respuesta exitosa
-    console.log("success");
-
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  });
-
-
-
-
-  axios.get('http://localhost:3000/icons/1')
-  .then(response => {
-      
-    const valores_Convertidos = response.data;
-    document.getElementById("icono1").innerHTML = valores_Convertidos.Icono;
-
-
-    // manejar respuesta exitosa
-    console.log("success");
-
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  });
-
 
   axios.get('http://localhost:3000/servicios/1')
   .then(response => {
       
     const valores_Convertidos = response.data;
+    document.getElementById("plan1").innerHTML = valores_Convertidos.Plan;
+    document.getElementById("resultado").innerHTML = valores_Convertidos.Precio;
+    document.getElementById("icono1").innerHTML = valores_Convertidos.Icono;
     document.getElementById("Texto").innerHTML = valores_Convertidos.Servicio;
 
 
@@ -83,78 +57,19 @@
     console.log(error);
   })
   .finally(function () {
-    // siempre sera executado
+    verificarPeticiones();
   });
 
 
   //modulo 2
 
-
-  
-   axios.get('http://localhost:3000/planes/2')
+    axios.get('http://localhost:3000/servicios/2')
   .then(response => {
       
     const valores_Convertidos = response.data;
     document.getElementById("plan2").innerHTML = valores_Convertidos.Plan;
-
-
-    // manejar respuesta exitosa
-    console.log("success");
-
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  });
-
-
-  axios.get('http://localhost:3000/precio/2')
-  .then(response => {
-      
-    const valores_Convertidos = response.data;
-    document.getElementById("resultado2").textContent = valores_Convertidos.Precio;
-
-
-    // manejar respuesta exitosa
-    console.log("success");
-
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  });
-
-
-   axios.get('http://localhost:3000/icons/2')
-  .then(response => {
-      
-    const valores_Convertidos = response.data;
+    document.getElementById("resultado2").innerHTML = valores_Convertidos.Precio;
     document.getElementById("icono2").innerHTML = valores_Convertidos.Icono;
-
-
-    // manejar respuesta exitosa
-    console.log("success");
-
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  });
-
-
-   axios.get('http://localhost:3000/servicios/2')
-  .then(response => {
-      
-    const valores_Convertidos = response.data;
     document.getElementById("Texto2").innerHTML = valores_Convertidos.Servicio;
 
 
@@ -167,82 +82,19 @@
     console.log(error);
   })
   .finally(function () {
-    // siempre sera executado
+    verificarPeticiones();
   });
 
 
   //modulo 3
 
-
-
-
-     axios.get('http://localhost:3000/planes/3')
-  .then(response => {
+    axios.get('http://localhost:3000/servicios/3')
+    .then(response => {
       
     const valores_Convertidos = response.data;
     document.getElementById("plan3").innerHTML = valores_Convertidos.Plan;
-
-
-    // manejar respuesta exitosa
-    console.log("success");
-
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  });
-
-
-
-
-
-  axios.get('http://localhost:3000/precio/3')
-  .then(response => {
-      
-    const valores_Convertidos = response.data;
-    document.getElementById("resultado3").textContent = valores_Convertidos.Precio;
-
-
-    // manejar respuesta exitosa
-    console.log("success");
-
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  });
-
-
-   axios.get('http://localhost:3000/icons/3')
-  .then(response => {
-      
-    
-    const valores_Convertidos = response.data;
+    document.getElementById("resultado3").innerHTML = valores_Convertidos.Precio;
     document.getElementById("icono3").innerHTML = valores_Convertidos.Icono;
-
-
-    // manejar respuesta exitosa
-    console.log("success");
-
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  });
-
-   axios.get('http://localhost:3000/servicios/3')
-  .then(response => {
-      
-    const valores_Convertidos = response.data;
     document.getElementById("Texto3").innerHTML = valores_Convertidos.Servicio;
 
 
@@ -255,5 +107,8 @@
     console.log(error);
   })
   .finally(function () {
-    // siempre sera executado
+    verificarPeticiones();
   });
+
+  
+});
